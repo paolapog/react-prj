035a36d3d257c1b3725d3802c1ca9fefe76051cd
paolapog <ppoggini@lifeed.io>
2021-03-06 16:21:28 +0100

    logic for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient' ;
 
 const burger = (props) => {
     //take the object ingredients and return it as an array
-    const transformIngredients = 
+    let transformIngredients = 
         Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
             });  
-        });
-
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if(transformIngredients.length === 0){
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
